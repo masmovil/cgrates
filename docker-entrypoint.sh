1b8c20db4c0d020fd33a5315e91bb76eac0a6b2d
Jose Domenech Leal <jose.domenech@bq.com>
2019-10-15 10:39:16 +0200

    CDR load config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,6 @@
 #! /usr/bin/env sh
 
-export PGPASSWORD="mysecretpassword"
-
 echo "Setup tables"
-
 cd ./data/storage/postgres
 ./setup_cgr_db.sh postgres postgres
 
@@ -17,4 +14,4 @@ sleep 3
 echo "-----------------------------------Loading bootstrap data-----------------------------------"
 cgr-loader -config_path=/etc/cgrates -path=/tmp/data/bootstrap -verbose
 
-tail -f /var/log/messages
+tail -f /dev/null
