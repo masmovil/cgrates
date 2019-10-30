ed5cdb7fd500be6c4562abf71e2caf8c7c1594da
Jose Domenech Leal <jose.domenech@bq.com>
2019-10-30 14:55:46 +0100

    Terraform trigger updated

--- infra/terraform/cloudbuild.tf
+++ infra/terraform/cloudbuild.tf
@@ -1,7 +1,7 @@
 resource "google_cloudbuild_trigger" "build-golang-branches" {
   project        = "mm-cloudbuild"
   description    = "Build cgrates branch"
-  included_files = ["./**"]
+  included_files = ["*/**"]
   trigger_template {
     branch_name = ".*"
     repo_name   = "github_masmovil_cgrates"
