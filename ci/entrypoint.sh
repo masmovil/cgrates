d61e8e05e8f17855ce5fc731df626be89652f947
Jose Domenech Leal <jose.domenech@bq.com>
2019-10-30 14:27:53 +0100

    Cloudbuild config

--- ci/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -Eeo pipefail
-
-exec "$@"
---
+++ infra/terraform/cloudbuild.tf
@@ -0,0 +1,11 @@
+resource "google_cloudbuild_trigger" "build-golang-branches" {
+  project        = "mm-cloudbuild"
+  description    = "Build cgrates branch"
+  included_files = ["./**"]
+  trigger_template {
+    branch_name = ".*"
+    repo_name   = "github_masmovil_cgrates"
+    dir         = "./"
+  }
+  filename = "cloudbuild.yaml"
+}
